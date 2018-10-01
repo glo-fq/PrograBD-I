7e4d46abb8e7a9ba338d74fc2b5f331ed0b5cfff
Gloriana Fernandez <glorianafernandezq@hotmail.com>
2018-10-01 12:01:27 -0600

    Agregar Estado y su DataAccessLayer con los métodos aún incompletos

---
+++ WebApplication4/Logica/Estado.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Logica {
+    public class Estado {
+        private int id;
+        private int idAdmin;
+        private int idTipoEvento;
+        private string direccionIP;
+        private DateTime timeStamp;
+        private string valorAntes;
+        private string valorDespues;
+        private int idEntidad;
+
+        public Estado() {
+            Id = 0;
+            IdAdmin = 0;
+            IdTipoEvento = 0;
+            DireccionIP = "";
+            TimeStamp = new DateTime(1970, 1, 1); //Se pone un valor por defecto
+            ValorAntes = "";
+            ValorDespues = "";
+            IdEntidad = 0;
+        }
+
+        public Estado(int pId, int pIdAdmin, int pIdTipoEvento, string pDireccionIP, DateTime pTimeStamp, string pValorAntes, string pValorDespues, int pIdEntidad) {
+            Id = pId;
+            IdAdmin = pIdAdmin;
+            IdTipoEvento = pIdTipoEvento;
+            DireccionIP = pDireccionIP;
+            TimeStamp = pTimeStamp;
+            ValorAntes = pValorAntes;
+            ValorDespues = pValorDespues;
+            IdEntidad = pIdEntidad;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int IdAdmin { get => idAdmin; set => idAdmin = value; }
+        public int IdTipoEvento { get => idTipoEvento; set => idTipoEvento = value; }
+        public string DireccionIP { get => direccionIP; set => direccionIP = value; }
+        public DateTime TimeStamp { get => timeStamp; set => timeStamp = value; }
+        public string ValorAntes { get => valorAntes; set => valorAntes = value; }
+        public string ValorDespues { get => valorDespues; set => valorDespues = value; }
+        public int IdEntidad { get => idEntidad; set => idEntidad = value; }
+    }
+}
---
+++ WebApplication4/Logica/EstadoDataAccessLayer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication4.Logica {
+    public class EstadoDataAccessLayer {
+        string connectionString = "Persist Security Info=False;Integrated Security = SSPI;database=BDProgra;server=LAPTOP-O7727F54\\SQLEXPRESS01";
+
+        //Get todos los estados
+        public IEnumerable<Estado> GetAllEstados() {
+            List<Estado> listaEstados = new List<Estado>();
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+                SqlCommand cmd = new SqlCommand("CASP_GetEstados", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) {
+                    Estado estado = new Estado();
+
+                    listaClientes.Add(cliente);
+                }
+                con.Close();
+            }
+
+            return listaEstados;
+        }
+
+        //Agregar un estado
+
+        //Editar o borrar un estado ???
+    }
+}
